a0587d66e5bb2fbf3e36eca7e15ade026f229dc5
Salma Rafik <srmb220@gmail.com>
2026-02-12 22:35:34 +0200

    first commit

---
+++ styles/_1_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style__1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10.4px \'Open Sans\', sans-serif";
+    var labelFill = "#5c5c5c";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("اسم_ا") !== null) {
+        labelText = String(feature.get("اسم_ا"));
+    }
+    var style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(148,209,128,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
